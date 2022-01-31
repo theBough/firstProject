7a3f9f92a8e9413281b4a004f1574ca6fec2b018
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-01-31 10:33:08 -0600

    Update player.js

--- player.js
+++ player.js
@@ -3,9 +3,19 @@ function Player(x,y,w,h,col){
   this.y = y;
   this.w = w;
   this.h = h;
+  this.speed = 5;
   
   this.display = function() {
     fill(col);
     rect(this.x, this.y , this.w, this.h)
-  }//end display    
+  }//end display 
+  
+  this.update = function(){
+    if(keyIsDown(39)){
+      //the arrow is clicked
+      this.x += this.speed
+    }
+    //you need an if block for each direction
+    
+  }//end function
 }//end Player
