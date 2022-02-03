2cb668d6472a021826e2c1fdd32db4b31eb72853
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-02-03 10:35:45 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,18 @@
 let p;
-let w;
+let w = [];
 function setup() {
   createCanvas(500, 400);
   p = new Player(200,200,50,50,"green");
-  w = new Wall(100,100,200,10,"yellow");
+  w.push(new Wall(100,100,200,10,"yellow"));
+  w.push(new Wall(100,200,200,10,"yellow"));
 }
 
 function draw() {
   background(102,0,204);
   p.display();
   p.update();
-  w.display()
+  for(var i=0 ; i < w.length ; i++){
+    w[i].display();  
+  }
+  
 }
