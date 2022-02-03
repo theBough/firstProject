2ed09ed3b9d63a9ea281fefffedd0f9e36ad9f9b
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-02-03 10:24:26 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,14 @@
 let p;
+let w;
 function setup() {
   createCanvas(500, 400);
-  p = new Player(200,200,50,50,"green")
+  p = new Player(200,200,50,50,"green");
+  w = new Wall(100,100,200,10,"yellow");
 }
 
 function draw() {
   background(102,0,204);
   p.display();
   p.update();
+  w.display()
 }
