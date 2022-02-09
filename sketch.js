a75bf9cb15f3f7c7219fcdaa0773ea17129c0a38
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-02-09 11:00:39 -0600

    This has the Collision in it.
    Just grab the function checkForCollission()
    And line 6.

--- sketch.js
+++ sketch.js
@@ -13,9 +13,36 @@ function draw() {
   for(var i=0 ; i < w.length ; i++){
     w[i].display();  
   }
+  checkForCollission();
   
 }
 function createWalls(){
   w.push(new Wall(100,100,200,10,"yellow"));
   w.push(new Wall(100,200,200,10,"yellow"));
+}
+
+function checkForCollission() {
+  for (var i = 0; i < w.length; i++) {
+
+    //check if we hit the left of any wall
+    if (p.y <= w[i].y + w[i].h && p.y + p.h >= w[i].y && p.x <= w[i].x + w[i].w && p.x >= w[i].x) {
+      p.x += 5
+    }
+
+    //check if we hit the right of any wall
+    if (p.y <= w[i].y + w[i].h && p.y + p.h >= w[i].y && p.x + p.w >= w[i].x && p.x <= w[i].x + w[i].w) {
+      p.x -= 5
+    }
+
+    //check if we hit the bottom of any wall
+    if (p.x <= w[i].x + w[i].w && p.x + p.w >= w[i].x && p.y <= w[i].y + w[i].h && p.y >= w[i].y) {
+      p.y += 5;
+    }
+
+    //check if we hit the top of any wall
+    if (p.x <= w[i].x + w[i].w && p.x + p.w >= w[i].x && p.y + p.h >= w[i].y && p.y <= w[i].y + w[i].h) {
+      p.y -= 5;
+    }
+
+  }
 }
