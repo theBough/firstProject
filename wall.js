161e32b6222e0a1156b2e7a061e46c50d9f8d334
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-02-09 09:43:29 -0600

    Update wall.js

--- wall.js
+++ wall.js
@@ -5,6 +5,8 @@ function Wall(x,y,w,h, col){
   this.h = h;
   
   this.display = function(){
+    //use push and the mathcing pop to apply
+    //the fill property to just this wall.
     push()
     fill(col);
     rect(this.x, this.y, this.w, this.h);
