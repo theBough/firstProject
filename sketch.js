5b3d872d2f2e3edff8748b0a413efaac12a98b16
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-02-14 10:24:26 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
 let p;
 let w = [];
+let room = 1;
 function setup() {
   createCanvas(500, 400);
   p = new Player(200,200,50,50,"green");
-  createWalls();
+  drawRoomOne();
 }
-
 function draw() {
   background(102,0,204);
   p.display();
@@ -14,12 +14,34 @@ function draw() {
     w[i].display();  
   }
   checkForCollission();
-  
+  changeScreenTime();  
 }
-function createWalls(){
-  w.push(new Wall(100,100,200,10,"yellow"));
-  w.push(new Wall(100,200,200,10,"yellow"));
-}
+function whereTo(direction){
+  w = [];
+  switch(room){
+    case 1:
+      //in room 1
+      if(direction == "up"){
+        //the player has gone up from room number 1
+        room = 2;
+        drawRoomTwo();
+      }//end if
+      break;
+    case 2:
+      
+      break;
+  }//end switch
+}//end whereTo
+function changeScreenTime(){
+  //this function will check if the player has gone off 
+  //the canvas
+  if(p.y<0){
+    //check to see if player went off the top
+    p.y = height-p.h
+    whereTo("up");
+  }//end if
+}//end changeScreenTime
+
 
 function checkForCollission() {
   for (var i = 0; i < w.length; i++) {
