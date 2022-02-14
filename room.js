c760901f37623884ed8314837211b5fa949a93bf
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-02-14 10:25:06 -0600

    Create room.js

---
+++ room.js
@@ -0,0 +1,8 @@
+function drawRoomOne(){
+  w.push(new Wall(100,100,200,10,"yellow"));
+  w.push(new Wall(100,200,200,10,"yellow"));
+}
+
+function drawRoomTwo(){
+  w.push(new Wall(300,300,10,100,"green"));
+}
