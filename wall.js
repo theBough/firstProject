d19599035b0850da053634bcc18b9124ee93b8c3
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-02-16 11:05:19 -0600

    Update wall.js
    Moved checkForCollision() into Wall.js

--- wall.js
+++ wall.js
@@ -5,11 +5,36 @@ function Wall(x,y,w,h, col){
   this.h = h;
   
   this.display = function(){
-    //use push and the mathcing pop to apply
-    //the fill property to just this wall.
     push()
     fill(col);
     rect(this.x, this.y, this.w, this.h);
     pop()
+  }
+
+}
+
+ function checkForCollission() {
+  for (var i = 0; i < w.length; i++) {
+
+    //check if we hit the left of any wall
+    if (p.y <= w[i].y + w[i].h && p.y + p.h >= w[i].y && p.x <= w[i].x + w[i].w && p.x >= w[i].x) {
+      p.x += 5
+    }
+
+    //check if we hit the right of any wall
+    if (p.y <= w[i].y + w[i].h && p.y + p.h >= w[i].y && p.x + p.w >= w[i].x && p.x <= w[i].x + w[i].w) {
+      p.x -= 5
+    }
+
+    //check if we hit the bottom of any wall
+    if (p.x <= w[i].x + w[i].w && p.x + p.w >= w[i].x && p.y <= w[i].y + w[i].h && p.y >= w[i].y) {
+      p.y += 5;
+    }
+
+    //check if we hit the top of any wall
+    if (p.x <= w[i].x + w[i].w && p.x + p.w >= w[i].x && p.y + p.h >= w[i].y && p.y <= w[i].y + w[i].h) {
+      p.y -= 5;
+    }
+
   }
 }
