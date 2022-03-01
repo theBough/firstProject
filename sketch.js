7049dfe03d0158c3fb932837ccf85d20d184d366
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-03-01 10:35:47 -0600

    Update sketch.js
    Moved to Array like movement

--- sketch.js
+++ sketch.js
@@ -1,63 +1,48 @@
 let p;
 let w = [];
-let room = 1;
+let rooms
+let row,column;
 function setup() {
   createCanvas(500, 400);
-  p = new Player(200,200,50,50,"green");
-  drawRoomOne();
+  p = new Player(200,200,50,50,"#ffff33");
+  row = 0;
+  column = 0;  
+  drawRoom();
 }
 function draw() {
-  background(102,0,204);
+  background("#33ccff");
   p.display();
   p.update();
+
   for(var i=0 ; i < w.length ; i++){
     w[i].display();  
   }
+  rooms[row][column].call();
   checkForCollission();
   changeScreenTime();  
 }
-function whereTo(direction){
-  w = [];
-  switch(room){
-    case 1:
-      //in room 1
-      if(direction == "up"){
-        //the player has gone up from room number 1
-        room = 2;
-        drawRoomTwo();
-      }//end if
-      break;
-    case 2:
-      //the player is in room 2
-      if(direction == "down"){
-        room=1
-        drawRoomOne();
-      }
-      break;
-  }//end switch
-}//end whereTo
 function changeScreenTime(){
   //this function will check if the player has gone off 
   //the canvas
   if(p.y<0){
     //check to see if player went off the top
     p.y = height-p.h
-    whereTo("up");
+    row -= 1;
   }//end if
   if(p.y > height){
     //check to see if player went past the bottom
     p.y =0
-    whereTo("down");
+    row += 1;
   }//end if
   if(p.x<0){
     //check to see if player went off to the left
     p.x = width-p.w
-    whereTo("left");
+   column -= 1;
   }//end if
   if(p.x > width){
     //check to see if player went off the right
     p.x = 0
-    whereTo("right")
+   column+=1;
   }//end if
 }//end changeScreenTime
 
