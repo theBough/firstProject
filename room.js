1f1fe8fc816e9108b63e6e11300e4be235dada99
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-03-01 10:37:02 -0600

    Update room.js
    We changed the DrawRoom() function.
    Don't copy my rooms because you will lose your creations,

--- room.js
+++ room.js
@@ -1,8 +1,19 @@
-function drawRoomOne(){
-  w.push(new Wall(100,100,200,10,"yellow"));
-  w.push(new Wall(100,200,200,10,"yellow"));
-}
 
-function drawRoomTwo(){
-  w.push(new Wall(300,300,10,100,"green"));
+function drawRoom() {
+ rooms = [
+      [startRoom,blankRoom]
+   ]
+}//end function
+
+var startRoom = function() {
+  w = [];
+  w.push(new Wall(100, 100, 200, 10, "#b3b300"));
+  w.push(new Wall(100, 200, 200, 10, "#b3b300"));
 }
+var keyRoom = function() {
+  w.push(new Wall(300, 300, 10, 100, "#b3b300"));
+}
+var blankRoom = function(){
+  w = [];
+  w.push(new Wall(75,75,200,200,"pink"))
+}//end blankRoom
