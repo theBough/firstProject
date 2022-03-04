d783ba19c9b057f38581b4b46ab0e492dd787c68
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-03-04 08:53:51 -0600

    Made a 9 room matrix

--- room.js
+++ room.js
@@ -1,22 +1,61 @@
-//if you have started making your own rooms, then
-//just copy the "drawRoom()" function
-//note: the name of the rooms on line 7
-//needs to match what you called them down below
+/*If you have already started designing your rooms, 
+be sure to just copy the drawRoom() function.
+The function calls in the "rooms" array, need to match the 
+names of your draw room function names.
+*/
 function drawRoom() {
- rooms = [
-      [startRoom,blankRoom]
-   ]
-}//end function
+  rooms = [
+    [blankRoom, dragonRoom, blankRoom],
+    [keyRoom, startRoom, swordRoom],
+    [blankRoom, bossRoom, blankRoom],
+  ];
+} //end function
 
-var startRoom = function() {
+var startRoom = function () {
   w = [];
-  w.push(new Wall(100, 100, 200, 10, "#b3b300"));
-  w.push(new Wall(100, 200, 200, 10, "#b3b300"));
-}
-var keyRoom = function() {
-  w.push(new Wall(300, 300, 10, 100, "#b3b300"));
-}
-var blankRoom = function(){
+  w.push(new Wall(0, 0, 150, 20, "#b3b300"));
+  w.push(new Wall(width - 150, 0, 150, 20, "#b3b300"));
+  w.push(new Wall(0, 0, 20, 150, "#b3b300"));
+  w.push(new Wall(0, height - 150, 20, 150, "#b3b300"));
+  w.push(new Wall(0, height - 20, 150, 20, "#b3b300"));
+  w.push(new Wall(width - 150, height - 20, 150, 20, "#b3b300"));
+  w.push(new Wall(width - 20, 0, 20, 150, "#b3b300"));
+  w.push(new Wall(width - 20, height - 150, 20, 150, "#b3b300"));
+};
+var bossRoom = function () {
   w = [];
-  w.push(new Wall(75,75,200,200,"pink"))
-}//end blankRoom
+  w.push(new Wall(0, 0, 150, 20, "#b3b300"));
+  w.push(new Wall(width - 150, 0, 150, 20, "#b3b300"));
+  w.push(new Wall(0, 0, 20, height, "#b3b300"));
+  w.push(new Wall(0, height - 20, width, 20, "#b3b300"));
+  w.push(new Wall(width - 20, 0, 20, height, "#b3b300"));
+
+};
+var swordRoom = function () {
+  w = [];
+  w.push(new Wall(0, 0, width, 20, "#b3b300"));
+  w.push(new Wall(0, 0, 20, 150, "#b3b300"));
+  w.push(new Wall(0, height - 150, 20, 150, "#b3b300"));
+  w.push(new Wall(0, height - 20, width, 20, "#b3b300"));
+  w.push(new Wall(width - 20, 0, 20, height, "#b3b300"));
+};
+var keyRoom = function () {
+  w = [];
+  w.push(new Wall(0, 0, width, 20, "#b3b300"));
+  w.push(new Wall(0, 0, 20, height, "#b3b300"));
+  w.push(new Wall(0, height - 20, width, 20, "#b3b300"));
+  w.push(new Wall(width - 20, 0, 20, 150, "#b3b300"));
+  w.push(new Wall(width - 20, height - 150, 20, 150, "#b3b300"));
+};
+var dragonRoom = function () {
+  w = [];
+  w.push(new Wall(0, 0, width, 20, "#b3b300"));
+  w.push(new Wall(0, 0, 20, height, "#b3b300"));
+  w.push(new Wall(0, height - 20, 150, 20, "#b3b300"));
+  w.push(new Wall(width - 150, height - 20, 150, 20, "#b3b300"));
+  w.push(new Wall(width - 20, 0, 20, height, "#b3b300"));
+};
+var blankRoom = function () {
+  w = [];
+  w.push(new Wall(75, 75, 200, 200, "pink"));
+}; //end blankRoom
