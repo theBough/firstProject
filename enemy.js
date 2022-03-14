cd175fb331f01081dc9be3db3729a2870fcdab6b
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-03-14 10:11:12 -0500

    Added Movement

--- enemy.js
+++ enemy.js
@@ -3,13 +3,22 @@ function Enemy(x,y,w,h){
   this.y = y
   this.w = w;
   this.h = h;
+  this.xSpeed = 3;
+  this.ySpeed = 3;
   //make sure the picture is in 
   //the directory on the left
   this.img = loadImage("rightFlat.png")
   
   this.display = function(){
+    this.img.resize(this.w, this.h)
     image(this.img,this.x, this.y);
   }//end display
+  this.update = function(){
+    //update the location of the Enemey.
+    //change the y location and x location
+    this.x += this.xSpeed;
+    this.y += this.ySpeed;
+  }//end update
   
   
 }//end Enemy
