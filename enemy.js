6875b66433b6cf1615d8a283cc506515cc12d74e
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-03-14 11:38:01 -0500

    The enemy should now move.

--- enemy.js
+++ enemy.js
@@ -1,34 +1,40 @@
 function Enemy(x,y,w,h){
-  this.x =x;
-  this.y = y
+  this.x = x;
+  this.y = y;
   this.w = w;
   this.h = h;
-  this.xSpeed = 1;
+  this.img = loadImage("rightFlat.png");
+  this.xSpeed  =1;
   this.ySpeed = 1;
-  //make sure the picture is in 
-  //the directory on the left
-  this.img = loadImage("rightFlat.png")
   
   this.display = function(){
     this.img.resize(this.w, this.h)
-    image(this.img,this.x, this.y);
+    image(this.img, this.x, this.y);
   }//end display
+  
   this.update = function(){
-    //update the location of the Enemey.
-    //change the y location and x location
+    /*This function will move the enemy
+    and move the Enemy base on where the player is.
+    */
     this.x += this.xSpeed;
     this.y += this.ySpeed;
     if(this.x > p.x){
+      //the Enemy is on the right side of 
+      //the player
       this.xSpeed = -1;
     }else{
-      this.xSpeed = 1;
+      /*The enemy is on the Left side of 
+      the player*/
+      this.xSpeed = 1
     }
     if(this.y > p.y){
+      //the Enemy is below 
+      //the player
       this.ySpeed = -1;
     }else{
-      this.ySpeed = 1;
+      /*The enemy is above  
+      the player*/
+      this.ySpeed = 1
     }
   }//end update
-  
-  
 }//end Enemy
